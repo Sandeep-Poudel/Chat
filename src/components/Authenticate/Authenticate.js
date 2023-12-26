cb5989fcb40b8e61bffa2661279c81c0c6a8af80
Sandeep-Poudel <poudelsandeep0105@gmail.com>
2023-12-26 19:52:55 +0545

    Added loading screen while loading

--- src/components/Authenticate/Authenticate.js
+++ src/components/Authenticate/Authenticate.js
@@ -3,15 +3,16 @@ import SignupForm from "./SignupForm";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-
+import LoadingScreen from "../Reusable/LoadingScreen";
 function Authenticate() {
     const [isLoginView, setIsLoginView] = useState(true);
-    const isLoggedin = useSelector((state) => state.user.isLoggedin);
     const toggleView = () => {
         setIsLoginView(!isLoginView); // Toggle the view state
     };
-    const { username, password, email } = useSelector((state) => {
+    const { isLoading, isLoggedin, username, password, email } = useSelector((state) => {
         return {
+            isLoding: state.user.isLoding,
+            isLoggedin: state.user.isLoggedin,
             username: state.form.username,
             password: state.form.password,
             email: state.form.email
@@ -23,8 +24,8 @@ function Authenticate() {
     return (
         <div className="flex items-center justify-center h-screen flex-col bg-gray-100" >
             <div className="w-[480px]">
+                <LoadingScreen isLoading={isLoading} />
                 {isLoginView ? (
-
                     <LoginForm password={password} email={email} toggleView={toggleView} />
                 ) : (
                     <SignupForm toggleView={toggleView} username={username} password={password} email={email} />
