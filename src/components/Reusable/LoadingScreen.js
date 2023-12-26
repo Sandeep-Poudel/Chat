a9dcdb5071fb946f2ac1ecb00035e215b1b0d5b1
Sandeep-Poudel <poudelsandeep0105@gmail.com>
2023-12-26 18:27:25 +0545

    Loading screen implemented

---
+++ src/components/Reusable/LoadingScreen.js
@@ -0,0 +1,14 @@
+import { GoSync } from "react-icons/go";
+
+const LoadingModal = ({ isLoading }) => (
+    <div className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ${isLoading ? 'block' : 'hidden'}`}>
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+            <div className="flex items-center justify-center mb-4">
+                <GoSync className="animate-spin text-4xl text-gray-800" />
+            </div>
+            <p className="text-center text-gray-800">Loading...</p>
+        </div>
+    </div>
+);
+
+export default LoadingModal;
